7c23eeb97734a6f72356388765a997b8354f6fac
Hiếu <anhhieuk50@gmail.com>
2025-05-22 09:26:48 +0700

    api oromi

---
+++ BioLife.API/Program.cs
@@ -0,0 +1,16 @@
+using HuloToys_Service;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();      
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+}
